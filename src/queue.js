3d034c36da3852d04bdba3e32edf2b366434ce15
mikalaiserhiyenia <mikalai.serhiyenia@gmail.com>
2025-11-10 19:52:06 +0300

    pass 28 fail 2

--- src/queue.js
+++ src/queue.js
@@ -1,5 +1,5 @@
 const { NotImplementedError } = require('../lib/errors');
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -12,7 +12,6 @@ const { NotImplementedError } = require('../lib/errors');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-
 class Queue {
   constructor() {
     this.head = null;
@@ -24,30 +23,26 @@ class Queue {
   }
 
   enqueue(value) {
-    const newNode = new ListNode(value);
-
+    const listNode = new ListNode(value);
     if (!this.head) {
-
-      this.head = newNode;
-      this.tail = newNode;
+      this.head = listNode;
+      this.tail = listNode;
     } else {
-
-      this.tail.next = newNode;
-      this.tail = newNode;
+      this.tail.next = listNode;
+      this.tail = listNode;
     }
   }
 
   dequeue() {
-    if (!this.head) return null;
-
-    const value = this.head.value;
+    if (!this.head) {
+      return undefined;
+    }
+    const dequeued = this.head.value;
     this.head = this.head.next;
-
     if (!this.head) {
       this.tail = null;
     }
-
-    return value;
+    return dequeued;
   }
 }
 
--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -23,21 +23,20 @@ const { ListNode } = require('../extensions/list-node.js');
  */
 
 function removeKFromList(l, k) {
-  while (l && l.value === k) {
-    l = l.next;
+  if (!l) {
+    return null;
   }
-
-  let current = l;
-
-  while (current && current.next) {
+  let listNode = new ListNode(0);
+  listNode.next = l;
+  let current = listNode;
+  while (current.next) {
     if (current.next.value === k) {
       current.next = current.next.next;
     } else {
       current = current.next;
     }
   }
-
-  return l;
+  return listNode.next;
 }
 
 module.exports = {
